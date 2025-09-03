042fec303a1e90dac5f7504d932d671eaaea2701
jhmoura <juliohmoura@pm.me>
2025-09-03 13:25:17 -0300

    second commit

--- src/Filme.java
+++ src/Filme.java
@@ -1,2 +1,23 @@
 public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double somaDasAvaliacoes;
+    int totalDeAvaliacao;
+    int duracaoEmMinutos;
+
+    void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lancamento: " + anoDeLancamento);
+
+    }
+
+    void avalia(double nota){
+        totalDeAvaliacao++;
+        somaDasAvaliacoes += nota;
+    }
+
+    double pegaMedia(){
+        return somaDasAvaliacoes / totalDeAvaliacao;
+    }
 }
---
+++ src/Principal.java
@@ -0,0 +1,17 @@
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoDeLancamento = 2021;
+        meuFilme.duracaoEmMinutos = 180;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(10);
+        meuFilme.avalia(9);
+
+        System.out.println(meuFilme.somaDasAvaliacoes);
+        System.out.println(meuFilme.totalDeAvaliacao);
+        System.out.println(meuFilme.pegaMedia());
+    }
+}
