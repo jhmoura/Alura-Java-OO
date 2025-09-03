ceea5f1c0294ca31fefff4e61b780eec94973740
jhmoura <juliohmoura@pm.me>
2025-09-03 15:56:54 -0300

    second commit

--- src/Principal.java
+++ src/Principal.java
@@ -1,17 +1,27 @@
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso chefão";
-        meuFilme.anoDeLancamento = 2021;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("Matrix");
+        meuFilme.setAnoDeLancamento(2001);
+        meuFilme.setDuracaoEmMinutos(150);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
         meuFilme.avalia(10);
         meuFilme.avalia(9);
 
-        System.out.println(meuFilme.somaDasAvaliacoes);
-        System.out.println(meuFilme.totalDeAvaliacao);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacao());
         System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("LOST");
+        lost.setAnoDeLancamento(2001);
+        lost.exibeFichaTecnica();
+        lost.setDuracaoEmMinutos(10);
+        lost.setEpisodiosPorTemporada(12);
+
     }
 }
---
+++ src/br/com/alura/screenmatch/modelos/Serie.java
@@ -0,0 +1,40 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Serie extends Titulo{
+    private int temporadas;
+    private int episodiosPorTemporada;
+    private boolean ative;
+    private int minutosPorEpisodio;
+
+    public int getTemporadas() {
+        return temporadas;
+    }
+
+    public void setTemporadas(int temporadas) {
+        this.temporadas = temporadas;
+    }
+
+    public int getEpisodiosPorTemporada() {
+        return episodiosPorTemporada;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada) {
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public boolean isAtive() {
+        return ative;
+    }
+
+    public void setAtive(boolean ative) {
+        this.ative = ative;
+    }
+
+    public int getMinutosPorEpisodio() {
+        return minutosPorEpisodio;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio) {
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+}
